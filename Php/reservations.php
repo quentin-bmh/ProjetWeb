2eb2cb666544f8316572b488e8226e460a78cdba
Beturan <bertrand.grandemange@orange.fr>
2024-02-09 14:45:43 +0100

    Link

--- Php/reservations.php
+++ Php/reservations.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="Style\reservation.css">
+    <script src="Script\reservations.js"></script>
 </head>
 <body>
     
