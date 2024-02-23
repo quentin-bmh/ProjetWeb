895175c7fd80402ed52fe514e2e613efaed84e84
Beturan <bertrand.grandemange@orange.fr>
2024-02-23 13:35:52 +0100

    t

---
+++ Script/carte.js
@@ -0,0 +1,7 @@
+function toggleContainers() {
+    var containerMenu = document.querySelector('.containermenu');
+    var container = document.querySelector('.container');
+
+    containerMenu.style.display = 'none';
+    container.style.display = 'block';
+}
--- carte.php
+++ carte.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="Style\cartes.css">
-    <script src="Script\cartes.js"></script>
+    <link rel="stylesheet" href="Style\carte.css">
+    <script src="Script\carte.js"></script>
 </head>
 <body>
     <?php include 'header.inc.php'; ?>
     <?php include 'footer.inc.php'; ?>
-    <div class="container">
+    <div class="containermenu" onclick="toggleContainers()">
         <div class="section" id="s1">
             <img src="Image\entree.jpg" alt="" style="width: auto; height: auto;">
         </div>
@@ -19,6 +19,14 @@
         </div>
         <div class="section" id="s3">
             <img src="Image\dessert.jpg" alt="" style="width: auto; height: auto;">
+        </div>
+    </div>
+    <div class="container">
+        <div class="blocHorizontal">
+            <div class="slide one">Slide 1</div>
+            <div class="slide two">Slide 2</div>
+            <div class="slide three">Slide 3</div>
+            <div class="slide four">Slide 4</div>
         </div>
     </div>
 </body>
