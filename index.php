fd3e0f619315e88d077120a5fe3dbc8a11b65890
Beturan <bertrand.grandemange@orange.fr>
2024-03-21 23:49:15 +0100

    blur

--- index.php
+++ index.php
@@ -10,9 +10,32 @@
 </head>
 <body>
     <?php include 'header.inc.php'; ?>
+    
     <video id="video" autoplay muted loop>
         <source src="Video/video.mp4" type="video/mp4">
         Your browser does not support the video tag.
     </video>
+
+<div class='bonjour'>
+    <?php 
+        if(isset($_SESSION['mail'])) {
+            $mail = $_SESSION['mail'];
+            try{
+                $mysqlConnection = new PDO(
+                    'mysql:host=localhost;dbname=projetweb;charset=utf8',
+                    'root', '', [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION],);  
+            }
+            catch (Exception $e){
+                var_dump('Erreur : ' . $e->getMessage());
+            }
+
+            $maTableStatement = $mysqlConnection->prepare("SELECT * FROM compte WHERE mail=:mail");
+            $maTableStatement->execute(["mail"=>$mail]);
+            $donnees = $maTableStatement->fetchAll()[0];
+            echo "<div class='bienvenue'>Bienvenue {$donnees['prenom']}</div>";
+            //echo "<div class='bonjour'>Bienvenue {$resultat['prenom']}</div>";
+        }
+    ?>  
+</div>
 </body>
 </html>
