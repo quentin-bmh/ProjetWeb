812d53abf1e1b04b93072b57a773294edf0b891e
Beturan <bertrand.grandemange@orange.fr>
2024-03-26 22:30:24 +0100

    oups

--- admin.php
+++ admin.php
@@ -33,19 +33,20 @@
                 $sql = $bdd->prepare('SELECT * FROM reservation AS a INNER JOIN compte AS c ON c.mail = a.mail');
                 $sql->execute();
                 $rep = $sql->fetchAll();
-                var_dump($rep);
+                //var_dump($rep);
                 foreach($rep as $line){
                     echo "<tr>";
                     //echo "<tr data-id='".$line["id"].""."'>";
                     echo "<td>".$line['date']."</td>";
                     echo "<td>".$line['heure']."</td>";
                     echo "<td>".$line['nbPers']." pers</td>";
-                    //echo "<td>".$line['nom']."</td>";
+                    echo "<td>".$line['nom']."</td>";
                     echo "<td>".$line['prenom']."</td>";
                     echo "<td>".$line['mail']."</td>";
                     echo "<td><a href='admin.php?del=".$line["id"]."'>"."X"."</a></td>";
                     echo "</tr>";
                 }
+                
                 //$sql->execute(['day' => $days, 'hour' => $hours, 'nom' => $nom, 'email' => $email, 'phone' => $phone, 'nbrPers' => $nbrPers]);
             } catch(PDOException $e) {
                 echo "Erreur : " . $e->getMessage();
